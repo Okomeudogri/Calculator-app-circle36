87aad82c8cd247035e500bdacfc9d9dee106e809
favourchidinma <fcakagha9943@gmail.com>
2023-09-12 18:00:39 +0100

    commit message

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons= document.querySelectorAll('[data-number]') 
+const operationButtons= document.querySelectorAll('[data-operation]') 
+const equalsButton= document.querySelector('[data-equals]')
+const deleteButton= document.querySelector('[data-delete]')
+const allclearButton= document.querySelector('[data-all-clear]')
+const previousOperandTextElementButton= document.querySelector('[data-previous-operand]')
+const currentOperandTextElementButton= document.querySelector('[data-current-operand]')
