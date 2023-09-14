f5b8c30d7a1ae021e10b9aab41c6291cfa785c5d
Chibuokem Ukandu <130072804+kems18@users.noreply.github.com>
2023-09-14 17:22:15 +0100

    Fixed Bugs in JS using DOM for data

--- script.js
+++ script.js
@@ -1,67 +1,107 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const previousOperandTextElement = document.querySelector(
+    "[data-previous-operand]"
+  );
+  const currentOperandTextElement = document.querySelector(
+    "[data-current-operand]"
+  );
 
-class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-      this.previousOperandTextElement = previousOperandTextElement
-      this.currentOperandTextElement = currentOperandTextElement
-      this.clear()
-    }
-  
-    clear() {
-      this.currentOperand = ''
-      this.previousOperand = ''
-      this.operation = undefined
-    }
-  
-    delete() {
-      this.currentOperand = this.currentOperand.toString().slice(0, -1)
-    }
-  
-    appendNumber(number) {
-      if (number === '.' && this.currentOperand.includes('.')) return
-      this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
-  
-    chooseOperation(operation) {
-      if (this.currentOperand === '') return
-      if (this.previousOperand !== '') {
-        this.compute()
-      }
-      this.operation = operation
-      this.previousOperand = this.currentOperand
-      this.currentOperand = ''
-    }
-  
-    // compute() {
-    //   let computation
-    //   const prev = parseFloat(this.previousOperand)
-    //   const current = parseFloat(this.currentOperand)
-    //   if (isNaN(prev) || isNaN(current)) return
-    //   switch (this.operation) {
-    //     case '+':
-    //       computation = prev + current
-    //       break
-    //     case '-':
-    //       computation = prev - current
-    //       break
-    //     case '*':
-    //       computation = prev * current
-    //       break
-    //     case '÷':
-    //       computation = prev / current
-    //       break
-    //     default:
-    //       return
-    //   }
-    //   this.currentOperand = computation
-    //   this.operation = undefined
-    //   this.previousOperand = ''
-    // }
-  
+  let currentOperand = "";
+  let previousOperand = "";
+  let operation = undefined;
 
-// const numberButtons= document.querySelectorAll('[data-number]') 
-// const operationButtons= document.querySelectorAll('[data-operation]') 
-// const equalsButton= document.querySelector('[data-equals]')
-// const deleteButton= document.querySelector('[data-delete]')
-// const allclearButton= document.querySelector('[data-all-clear]')
-// const previousOperandTextElementButton= document.querySelector('[data-previous-operand]')
-// const currentOperandTextElementButton= document.querySelector('[data-current-operand]')
+  const numberButtons = document.querySelectorAll("[data-numbers]");
+  const operationButtons = document.querySelectorAll("[data-operation]");
+  const equalsButton = document.querySelector("[data-equals]");
+  const deleteButton = document.querySelector("[data-delete]");
+  const allClearButton = document.querySelector("[data-all-clear]");
+
+  numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      appendNumber(button.innerText);
+      updateDisplay();
+    });
+  });
+
+  operationButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      chooseOperation(button.innerText);
+      updateDisplay();
+    });
+  });
+
+  equalsButton.addEventListener("click", () => {
+    compute();
+    updateDisplay();
+  });
+
+  allClearButton.addEventListener("click", () => {
+    clear();
+    updateDisplay();
+  });
+
+  deleteButton.addEventListener("click", () => {
+    deleteLast();
+    updateDisplay();
+  });
+
+  function appendNumber(number) {
+    if (number === "." && currentOperand.includes(".")) return;
+    currentOperand += number;
+  }
+
+  function chooseOperation(op) {
+    if (currentOperand === "") return;
+    if (previousOperand !== "") {
+      compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = "";
+  }
+
+  function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch (operation) {
+      case "+":
+        computation = prev + current;
+        break;
+      case "-":
+        computation = prev - current;
+        break;
+      case "*":
+        computation = prev * current;
+        break;
+      case "÷":
+        computation = prev / current;
+        break;
+      default:
+        return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = "";
+  }
+
+  function clear() {
+    currentOperand = "";
+    previousOperand = "";
+    operation = undefined;
+  }
+
+  function deleteLast() {
+    currentOperand = currentOperand.toString().slice(0, -1);
+  }
+
+  function updateDisplay() {
+    currentOperandTextElement.innerText = currentOperand;
+    if (operation != null) {
+      previousOperandTextElement.innerText = `${previousOperand} ${operation}`;
+    } else {
+      previousOperandTextElement.innerText = "";
+    }
+  }
+});
